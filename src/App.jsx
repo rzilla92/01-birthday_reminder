b4a5e6681d43992f491bd94b9357d37fa0e87b1f
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-11-26 15:41:54 +0700

    completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import data from './data/data';
+import List from './List/List'
+import './App.css'
+
+function App() {
+  const [people, setPeople] = useState(data)
+  console.log(people.length)
+  return (
+    <main>
+      <section className='container'>
+        <h3> {people.length} birthdays today.</h3>
+        <List people={people}/>
+        <button>Clear All</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
